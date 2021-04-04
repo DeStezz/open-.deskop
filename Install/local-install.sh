f11734081f05d57514d4657e4d83763886c52acc
ServerBBQ <76498501+ServerBBQ@users.noreply.github.com>
2021-04-04 13:42:40 -0400

    Fixed bug
    Fixed bug that makes installer fail if `~/.local/bin` is not present

--- Install/local-install.sh
+++ Install/local-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd `dirname $0`
 echo "Copying script to ~/.local/bin"
+mkdir ~/.local/bin/ 2> /dev/null
 cp ../desktop-file-open.sh ~/.local/bin/desktop-file-open
 echo "Copying desktop file tp ~/.local/share/applications"
 cp ./Files/Local-install/desktop-file-open.desktop ~/.local/share/applications/.desktop-file-open.desktop
